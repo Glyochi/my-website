62a76977671d438dfd929149f3acbe9b7471fe96
Duong Vu <duongvh1806@gmail.com>
2022-01-06 11:02:58 -0600

    progress on facialDetection web app

--- src/components/Pages/Stream/StreamPageBody.js
+++ src/components/Pages/Stream/StreamPageBody.js
@@ -7,10 +7,10 @@ function StreamPageBody() {
         <div className="flex justify-center bg-green-500" >
             <div className=" flex flex-col justify-center items-center w-3/4 bg-purple-600">
                 <div>Toggles + Explaination</div>
-                <div className="videoContainer  w-3/4 aspect-video bg-blue-600">
+                {/* <div className="videoContainer  w-3/4  bg-blue-600"> */}
                     <VideoPlayer></VideoPlayer>
 
-                </div>
+                {/* </div> */}
                 <div>Stats + other stuffs</div>
 
             </div>
